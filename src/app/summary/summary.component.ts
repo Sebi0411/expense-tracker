92d86b93c3bfc1f8a1d0811e38c994af7a6d84dc
Sebi0411 <sebi041102@gmail.com>
2025-01-16 14:07:21 +0200

    added export to excel

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ExpensesService } from '../expenses/expenses.service';
+import * as XLSX from 'xlsx';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-summary',
@@ -10,6 +12,7 @@ import { ExpensesService } from '../expenses/expenses.service';
   styleUrl: './summary.component.css',
 })
 export class SummaryComponent {
+
   expensesByDay: { [key: string]: { category: string; amount: number }[] } = {};
   days: string[] = [];
   weeklyTotal: number = 0;
@@ -20,5 +23,24 @@ export class SummaryComponent {
     this.expensesByDay = this.expensesService.getExpensesByDay();
     this.days = Object.keys(this.expensesByDay);
     this.weeklyTotal = this.expensesService.getTotalAmount();
+  }
+
+  exportToExcel(): void {
+    const data: { Day: string; Category: string; Amount: number }[] = [];
+    for (const day of this.days) {
+      this.expensesByDay[day].forEach(expense => {
+        data.push({
+          Day: day,
+          Category: expense.category,
+          Amount: expense.amount,
+        });
+      });
+    }
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Weekly Expenses');
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    saveAs(blob, 'Weekly_Expenses.xlsx');
   }
 }
