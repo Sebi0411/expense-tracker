00474bdc43adf186fbba585aa336e00d679bbd8c
Sebi0411 <sebi041102@gmail.com>
2025-02-12 15:30:25 +0200

    small fix

--- src/app/expenses/expenses.component.ts
+++ src/app/expenses/expenses.component.ts
@@ -29,8 +29,6 @@ export class ExpensesComponent {
     this.expensesSubscription = this.expensesService.expenses$.subscribe(() => {
       this.updateDayTotal();
     });
-
-    this.updateDayTotal();
   }
 
   ngOnChanges() {
